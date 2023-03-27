d2fa4bc12d8bdbf14df1e37ab9ce701d7e5b36bb
Lisandro Peralta <lisandroperalta@gmail.com>
2023-03-27 12:39:39 -0300

    avanzando con chat gpt

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
+let texto = "";
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(innerWidth, innerHeight);
 }
 
 function draw() {
-  background(220);
+  background(125);
+  
+  // mostrar todas las teclas presionadas en la pantalla
+  textSize(32);
+  textAlign(CENTER);
+  text(mostrarTexto(texto), displayWidth/2, displayHeight/2);
+}
+
+function keyPressed() {
+  // si se presiona "Enter", borrar el texto
+  if (keyCode === ENTER) {
+    texto = "";
+  } else if (keyCode === BACKSPACE) {
+    // borrar el último carácter
+    texto = texto.slice(0, -1);
+  } else if (esLetra(key) || key === " ") {
+    // agregar la tecla presionada al texto si es letra o espacio
+    texto += key;
+  }
+  
+  // prevenir que la página haga scroll cuando se presiona una tecla
+  return false;
+}
+
+function esLetra(caracter) {
+  // verificar si el caracter es una letra
+  return /^[a-zA-Z]$/.test(caracter);
+}
+
+function mostrarTexto(texto) {
+  // mostrar solo los caracteres de letras y espacio del texto
+  return texto.replace(/[^a-zA-Z ]/g, "");
 }
